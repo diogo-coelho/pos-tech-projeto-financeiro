45dc7d61ac5792858bdfa378f6fd313abcc332ed
Diogo Coelho <diogooliveiracoelho@gmail.com>
2024-10-26 14:10:41 -0300

    Correções de css

---
+++ components/ui/Statement/index.tsx
@@ -0,0 +1,13 @@
+import './Statement.scss';
+
+const Statement = () => {
+  return (
+    <>
+      <section className="statement-container">
+        <p>Extrato</p>
+      </section>
+    </>
+  )
+}
+
+export default Statement;
